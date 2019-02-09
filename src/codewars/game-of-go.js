1fb469f7c268d6e47cbe560e700e883a87250c2f
shinima <842351815@qq.com>
2019-02-09 22:31:14 +0800

    完成 game-of-go

--- src/codewars/game-of-go.js
+++ src/codewars/game-of-go.js
@@ -1,30 +1,42 @@
-// TODO 还有一点小问题
+const EMPTY_PIECE = '.'
+const X_AXIS_LETTERS = 'ABCDEFGHJKLMNOPQRSTUVWXYZ' // A-Z except I, 25 letters
+const LETTER_TO_COL = new Map(Array.from(X_AXIS_LETTERS).map((letter, index) => [letter, index]))
+const HANDICAP_POSITION_MAP = {
+  9: ['7G', '3C', '3G', '7C', '5E'],
+  13: ['10K', '4D', '4K', '10D', '7G', '7D', '7K', '10G', '4G'],
+  19: ['16Q', '4D', '4Q', '16D', '10K', '10D', '10Q', '16K', '4K'],
+}
 
-const letters = 'ABCDEFGHJKLMNOPQRSTUVWXYZ' // A-Z except I, 25 letters
-const letterToCol = new Map(Array.from(letters).map((letter, index) => [letter, index]))
-const handicapPositions = {
-  size9: ['7G', '3C', '3G', '7C', '5E'],
-  size13: ['10K', '4D', '4K', '10D', '7G', '7D', '7K', '10G', '4G'],
-  size19: ['16Q', '4D', '4Q', '16D', '10K', '10D', '10Q', '16K', '4K'],
+const errors = {
+  invalidPosition(position) {
+    return `Invalid position - ${position}.`
+  },
+  placeOnAnOccupiedPosition() {
+    return 'Cannot place stone at an already occupied position.'
+  },
 }
 
 function isRival(p1, p2) {
   return (p1 === 'x' && p2 === 'o') || (p1 === 'o' && p2 === 'x')
 }
 
-function getPiece(turn) {
+function getMyPiece(turn) {
   if (turn === 'black') {
     return 'x'
-  } else {
+  } else if (turn === 'white') {
     return 'o'
+  } else {
+    throw new Error(`Invalid turn - ${turn}`)
   }
 }
 
 function getRivalPiece(turn) {
   if (turn === 'white') {
+    return 'x'
+  } else if (turn === 'black') {
     return 'o'
   } else {
-    return 'x'
+    throw new Error(`Invalid turn - ${turn}`)
   }
 }
 
@@ -38,54 +50,84 @@ function getCouldPlaceHandicap(height, width) {
   return height === width && (height === 9 || height === 13 || height === 19)
 }
 
-function parsePosition(position, height) {
-  assert(position.length === 2 || position.length === 3)
-  if (position.length === 3) {
-    const col = letterToCol.get(position[2])
-    const row = height - Number(position.slice(0, 2))
-    return { row, col }
-  } else {
-    // position.length === 2
-    const col = letterToCol.get(position[1])
-    const row = height - Number(position[0])
-    return { row, col }
-  }
-}
-
-function initBoard(height, width) {
-  const board = []
-  for (let row = 0; row < height; row++) {
-    board.push(new Array(width).fill('.'))
-  }
-  return board
-}
-
 class Go {
   constructor(height, width = height) {
     assert(height >= 1 && height <= 25, 'Height should be in range [1, 25]')
     assert(width >= 1 && width <= 25, 'Width should be in range [1, 25]')
+
     this.height = height
     this.width = width
     this.turn = 'black' // black | white
-    this.board = initBoard(height, width)
 
+    this._list = new Array(height * width).fill(EMPTY_PIECE)
     this._couldPlaceHandicap = getCouldPlaceHandicap(height, width)
-    this._lastPosition = { white: null, black: null }
     this._history = []
+
+    const left = position => {
+      const { row, col } = this._parseRowCol(position)
+      return col > 0 ? this._makePosition(row, col - 1) : null
+    }
+    const right = position => {
+      const { row, col } = this._parseRowCol(position)
+      return col < this.width - 1 ? this._makePosition(row, col + 1) : null
+    }
+    const up = position => {
+      const { row, col } = this._parseRowCol(position)
+      return row > 0 ? this._makePosition(row - 1, col) : null
+    }
+    const down = position => {
+      const { row, col } = this._parseRowCol(position)
+      return row < this.height - 1 ? this._makePosition(row + 1, col) : null
+    }
+    this._directionFuncs = [left, right, up, down]
   }
 
-  print() {
-    const axisYWidth = String(this.width).length
-    console.log('='.repeat(60))
-    console.log(' '.repeat(axisYWidth + 1) + Array.from(letters.slice(0, this.width)).join(' '))
+  get board() {
+    const result = []
     for (let row = 0; row < this.height; row++) {
-      const n = this.height - row
-      console.log([String(n).padStart(axisYWidth)].concat(this.board[row]).join(' '))
+      const line = []
+      for (let col = 0; col < this.width; col++) {
+        line.push(this._list[row * this.width + col])
+      }
+      result.push(line)
     }
+    return result
   }
 
   get size() {
     return { height: this.height, width: this.width }
+  }
+
+  _parse(position) {
+    const match = position.match(/^(\d\d?)([A-Z])$/)
+    if (match == null) {
+      return null
+    }
+    const lineNumber = Number(match[1]) // lineNumber is 1-based
+    if (lineNumber < 1 || lineNumber > this.height) {
+      return -1
+    }
+    const colLetter = match[2]
+    const col = LETTER_TO_COL.get(colLetter) // col is 0-based
+    if (col == null || col < 0 || col > this.width - 1) {
+      return -1
+    }
+    return (this.height - lineNumber) * this.width + col
+  }
+
+  _parseRowCol(position) {
+    const t = this._parse(position)
+    assert(t !== -1, errors.invalidPosition(position))
+    return {
+      row: Math.floor(t / this.width),
+      col: t % this.width,
+    }
+  }
+
+  _setPosition(position, piece) {
+    const t = this._parse(position)
+    assert(t !== -1, errors.invalidPosition(position))
+    this._list[t] = piece
   }
 
   rollback(steps = 1) {
@@ -96,47 +138,52 @@ class Go {
 
   _rollbackOneStep() {
     const record = this._history.pop()
-    assert(record != null, 'Empty history')
+    assert(record != null, 'Cannot rollback.')
     if (record.type === 'move') {
       this._toggleTurn()
-      const { row, col } = parsePosition(record.position, this.height)
-      this.board[row][col] = '.'
+      this._setPosition(record.position, EMPTY_PIECE)
+      const rivalPiece = getRivalPiece(record.turn)
       for (const capture of record.captures) {
-        const { row, col } = this._getRowCol(capture)
-        this.board[row][col] = getRivalPiece(record.turn)
+        this._setPosition(capture, rivalPiece)
       }
     } else if (record.type === 'pass') {
+      this._toggleTurn()
+    } else if (record.type === 'place-handicap-stones') {
+      record.positions.forEach(position => {
+        this._setPosition(position, EMPTY_PIECE)
+      })
       this._toggleTurn()
     } else {
       assert(false, 'Invalid history record')
     }
-    this._lastPosition = record.lastPosition
     this._couldPlaceHandicap = record.couldPlaceHandicap
   }
 
   handicapStones(number) {
-    assert(this._couldPlaceHandicap)
-    let positions
-    if (this.height === 9) {
-      positions = handicapPositions.size9
-    } else if (this.height === 13) {
-      positions = handicapPositions.size13
-    } else {
-      positions = handicapPositions.size19
-    }
-    assert(number >= 1 && number <= positions.length)
+    assert(this._couldPlaceHandicap, 'Cannot place handicap stones for now')
+    const positions = HANDICAP_POSITION_MAP[this.height]
+    assert(positions != null, 'Cannot find handicap positions in HANDICAP_POSITION_MAP')
+    assert(
+      number >= 1 && number <= positions.length,
+      `handicap stone number must be in range [1, ${positions.length}]`,
+    )
 
-    positions.slice(0, number).forEach(position => {
-      const { row, col } = parsePosition(position, this.height)
-      this.board[row][col] = 'x'
+    this._history.push({
+      type: 'place-handicap-stones',
+      positions: positions.slice(0, number),
     })
+
+    for (const position of positions.slice(0, number)) {
+      this._setPosition(position, getMyPiece(this.turn))
+    }
     this._couldPlaceHandicap = false
-    this.turn = 'white'
+    this._toggleTurn()
   }
 
   getPosition(position) {
-    const { row, col } = parsePosition(position, this.height)
-    return this.board[row][col]
+    const t = this._parse(position)
+    assert(t !== -1, errors.invalidPosition(position))
+    return this._list[t]
   }
 
   move(...positions) {
@@ -145,155 +192,68 @@ class Go {
     }
   }
 
-  _getT(row, col) {
-    return row * this.width + col
-  }
+  _bfs(position, piece) {
+    const prevPiece = this.getPosition(position)
+    this._setPosition(position, piece)
 
-  _getRowCol(t) {
-    const row = Math.floor(t / this.width)
-    const col = t % this.width
-    return { row, col }
-  }
-
-  _bfs(row, col, piece) {
     const land = new Set()
     const liberties = new Set()
-    const borders = new Set()
-    const boundaries = new Set()
 
     let front = new Set()
-    front.add(this._getT(row, col))
+    front.add(position)
     while (front.size > 0) {
       const nextFront = new Set()
-      for (const t of front) {
-        land.add(t)
-        const { row, col } = this._getRowCol(t)
+      for (const pos of front) {
+        land.add(pos)
 
-        if (col > 0) {
-          const leftPiece = this.board[row][col - 1]
-          const leftT = t - 1
-          if (leftPiece === piece) {
-            if (!land.has(leftT) && !front.has(leftT)) {
-              nextFront.add(leftT)
-            }
-          } else if (leftPiece === '.') {
-            liberties.add(leftT)
-          } else {
-            boundaries.add(leftT)
+        for (const directionFn of this._directionFuncs) {
+          const neighbor = directionFn(pos)
+          if (neighbor == null) {
+            continue
           }
-        } else {
-          borders.add({ side: 'left', row })
-        }
-
-        if (col < this.width - 1) {
-          const rightPiece = this.board[row][col + 1]
-          const rightT = t + 1
-          if (rightPiece === piece) {
-            if (!land.has(rightT) && !front.has(rightT)) {
-              nextFront.add(rightT)
+          const neighborPiece = this.getPosition(neighbor)
+          if (neighborPiece === piece) {
+            if (!land.has(neighbor) && !front.has(neighbor)) {
+              nextFront.add(neighbor)
             }
-          } else if (rightPiece === '.') {
-            liberties.add(rightT)
-          } else {
-            boundaries.add(rightT)
+          } else if (neighborPiece === EMPTY_PIECE) {
+            liberties.add(neighbor)
           }
-        } else {
-          borders.add({ side: 'right', row })
-        }
-
-        if (row > 0) {
-          const upPiece = this.board[row - 1][col]
-          const upT = t - this.width
-          if (upPiece === piece) {
-            if (!land.has(upT) && !front.has(upT)) {
-              nextFront.add(upT)
-            }
-          } else if (upPiece === '.') {
-            liberties.add(upT)
-          } else {
-            boundaries.add(upT)
-          }
-        } else {
-          borders.add({ side: 'up', col })
-        }
-
-        if (row < this.height - 1) {
-          const downPiece = this.board[row + 1][col]
-          const downT = t + this.width
-          if (downPiece === piece) {
-            if (!land.has(downPiece) && !front.has(downPiece)) {
-              nextFront.add(downT)
-            }
-          } else if (downPiece === '.') {
-            liberties.add(downT)
-          } else {
-            boundaries.add(downT)
-          }
-        } else {
-          borders.add({ side: 'down', col })
         }
       }
       front = nextFront
     }
 
-    return { land, liberties, boundaries, borders }
+    this._setPosition(position, prevPiece)
+    return { land, liberties }
   }
 
-  _isSuicide(row, col, turn) {
-    const { liberties } = this._bfs(row, col, getPiece(turn))
+  _isSuicide(position, piece) {
+    const { liberties } = this._bfs(position, piece)
     return liberties.size === 0
   }
 
-  _findCaptures(row, col, lastPiece) {
-    const t = this._getT(row, col)
-    const captures = new Set()
-    const addToCaptures = set => {
-      for (const item of set) {
-        captures.add(item)
-      }
-    }
+  _makePosition(row, col) {
+    return `${this.height - row}${X_AXIS_LETTERS[col]}`
+  }
 
-    if (col > 0) {
-      const leftPiece = this.board[row][col - 1]
-      if (isRival(leftPiece, lastPiece)) {
-        const { liberties, land } = this._bfs(row, col - 1, leftPiece)
-        assert(liberties.size >= 1)
-        if (liberties.size === 1) {
-          assert(liberties.has(t), `Only liberty must be at { row=${row}, col=${col} }`)
-          addToCaptures(land)
-        }
+  _findCaptures(position, lastPiece) {
+    const captures = new Set()
+
+    for (const directionFn of this._directionFuncs) {
+      const neighbor = directionFn(position)
+      if (neighbor == null) {
+        continue
       }
-    }
-    if (col < this.width - 1) {
-      const rightPiece = this.board[row][col + 1]
-      if (isRival(rightPiece, lastPiece)) {
-        const { liberties, land } = this._bfs(row, col + 1, rightPiece)
-        assert(liberties.size >= 1)
+      const neighborPiece = this.getPosition(neighbor)
+      if (isRival(neighborPiece, lastPiece)) {
+        const { liberties, land } = this._bfs(neighbor, neighborPiece)
+        assert(liberties.size >= 1, 'liberties.size must greater than or equal to 1')
         if (liberties.size === 1) {
-          assert(liberties.has(t), `Only liberty must be at { row=${row}, col=${col} }`)
-          addToCaptures(land)
-        }
-      }
-    }
-    if (row > 0) {
-      const upPiece = this.board[row - 1][col]
-      if (isRival(upPiece, lastPiece)) {
-        const { liberties, land } = this._bfs(row - 1, col, upPiece)
-        assert(liberties.size >= 1)
-        if (liberties.size === 1) {
-          assert(liberties.has(t), `Only liberty must be at { row=${row}, col=${col} }`)
-          addToCaptures(land)
-        }
-      }
-    }
-    if (row < this.height - 1) {
-      const upPiece = this.board[row + 1][col]
-      if (isRival(upPiece, lastPiece)) {
-        const { liberties, land } = this._bfs(row + 1, col, upPiece)
-        assert(liberties.size >= 1)
-        if (liberties.size === 1) {
-          assert(liberties.has(t), `Only liberty must be at { row=${row}, col=${col} }`)
-          addToCaptures(land)
+          assert(liberties.has(position), `Only liberty must be at ${position}`)
+          for (const item of land) {
+            captures.add(item)
+          }
         }
       }
     }
@@ -301,37 +261,44 @@ class Go {
     return captures
   }
 
-  _executeCaptures(captures) {
-    for (const t of captures) {
-      const { row, col } = this._getRowCol(t)
-      this.board[row][col] = '.'
+  _isSatisfyKoRules(position, captures) {
+    if (this._history.length > 0) {
+      const record = this._history[this._history.length - 1]
+      if (
+        record.type === 'move' &&
+        record.captures.size === 1 &&
+        record.captures.has(position) &&
+        captures.size === 1 &&
+        captures.has(record.position)
+      ) {
+        return false
+      }
     }
+    return true
   }
 
   _moveOne(position) {
-    const { row, col } = parsePosition(position, this.height)
-    assert(0 <= row && row < this.height)
-    assert(0 <= col && col < this.width)
-    assert(this.board[row][col] === '.')
-
-    const piece = getPiece(this.turn)
-    const captures = this._findCaptures(row, col, piece)
-    assert(captures.size > 0 || !this._isSuicide(row, col, this.turn))
-    assert(position !== this._lastPosition[this.turn], 'Not satisfy KO rule')
+    assert(this.getPosition(position) === EMPTY_PIECE, errors.placeOnAnOccupiedPosition())
+    // position must be valid here
+    const piece = getMyPiece(this.turn)
+    const captures = this._findCaptures(position, piece)
+    assert(captures.size > 0 || !this._isSuicide(position, piece), 'Suicide is not allowed')
+    assert(this._isSatisfyKoRules(position, captures), 'Does not satisfy KO rule')
 
     this._history.push({
       type: 'move',
       turn: this.turn,
       captures,
       position,
-      lastPosition: { ...this._lastPosition },
       couldPlaceHandicap: this._couldPlaceHandicap,
     })
 
     this._couldPlaceHandicap = false
-    this.board[row][col] = piece
-    this._lastPosition[this.turn] = position
-    this._executeCaptures(captures)
+    this._setPosition(position, piece)
+    // executing captures
+    for (const pos of captures) {
+      this._setPosition(pos, EMPTY_PIECE)
+    }
     this._toggleTurn()
   }
 
@@ -346,8 +313,7 @@ class Go {
   reset() {
     this.turn = 'black'
     this._history = []
-    this._lastPosition = { black: null, white: null }
-    this.board = initBoard(this.height, this.width)
+    this._list = new Array(this.height * this.width).fill(EMPTY_PIECE)
     this._couldPlaceHandicap = getCouldPlaceHandicap(this.height, this.width)
   }
 
@@ -355,10 +321,21 @@ class Go {
     this._history.push({
       type: 'pass',
       turn: this.turn,
-      lastPosition: { ...this._lastPosition },
       couldPlaceHandicap: this._couldPlaceHandicap,
     })
     this._couldPlaceHandicap = false
     this._toggleTurn()
+  }
+
+  print() {
+    const axisYWidth = String(this.width).length
+    console.log('='.repeat(60))
+    console.log(
+      ' '.repeat(axisYWidth + 1) + Array.from(X_AXIS_LETTERS.slice(0, this.width)).join(' '),
+    )
+    for (let row = 0; row < this.height; row++) {
+      const n = this.height - row
+      console.log([String(n).padStart(axisYWidth)].concat(this.board[row]).join(' '))
+    }
   }
 }
